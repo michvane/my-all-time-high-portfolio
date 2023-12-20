822c4881b9ac596217a49174982a492a9980ea55
Michiel Van Eetvelde <35614140+michvane@users.noreply.github.com>
2023-12-21 00:37:51 +0100

    Update route.ts

--- src/app/api/currencies/route.ts
+++ src/app/api/currencies/route.ts
@@ -1,16 +1,16 @@
 import { crypto } from "../../../../data/crypto";
 
 export async function GET() {
-  const response = await fetch(
-    `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false&locale=en&x_cg_demo_api_key=${process.env.COINGECKO_API_KEY}`,
-    { cache: "force-cache", next: { revalidate: 600 } }
-  );
+  // const response = await fetch(
+  //  `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false&locale=en&x_cg_demo_api_key=${process.env.COINGECKO_API_KEY}`,
+  //  { cache: "force-cache", next: { revalidate: 600 } }
+  // );
 
-  const data = await response.json();
+  // const data = await response.json();
 
-  if (data?.status?.error_code === 429) {
+  // if (data?.status?.error_code === 429) {
     return Response.json(crypto);
-  }
+  // }
 
-  return Response.json({ data });
+  // return Response.json({ data });
 }
